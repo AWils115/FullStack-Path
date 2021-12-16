fa68ee52c99204140558fe8f1b89aeeeee5c25c7
AWils115 <wilsonalex23@googlemail.com>
2021-12-16 21:09:59 +0000

    Added menu button for small screens

---
+++ Club Website/Resources/script.js
@@ -0,0 +1,15 @@
+const myFunction = () => {
+	document.getElementById('my-dropdown').classList.toggle('show');
+};
+
+window.onclick = function (event) {
+	if (!event.target.matches('.menuIcon')) {
+		var dropdowns = document.getElementsByClassName('dropdown-content');
+		for (let i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+};
