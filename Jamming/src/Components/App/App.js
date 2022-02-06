eadc7ddc67ca2ec295b46feffd620f3863d80a5b
AWils115 <wilsonalex23@googlemail.com>
2022-02-06 10:20:50 +0000

    obtain spotify access token

--- Jamming/src/Components/App/App.js
+++ Jamming/src/Components/App/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -43,12 +44,15 @@ class App extends React.Component {
   savePlaylist(){
     const trackUris = this.state.playlistTracks.map(track => track.uri)
   }
+  search(searchTerm){
+    console.log(searchTerm);
+  }
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          {/*<SearchBar />*/}
+          <SearchBar onSearch={this.search} />
         <div className="App-playlist">
           <SearchResults SearchResults={this.state.SearchResults} onAdd={this.addTrack} />
           <Playlist playlistName={this.state.playlistName}
