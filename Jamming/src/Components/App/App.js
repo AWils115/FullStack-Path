656cb35c810c301e831f3990a0b6806d700db463
AWils115 <wilsonalex23@googlemail.com>
2022-02-06 09:01:20 +0000

    pass down playlist to tracklist

--- Jamming/src/Components/App/App.js
+++ Jamming/src/Components/App/App.js
@@ -1,5 +1,6 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
@@ -11,7 +12,13 @@ class App extends React.Component {
       SearchResults: [
         {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
         {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-        {name: 'name3', artist: 'artist3', album: 'album3', id: 3}] };
+        {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+      playlistName: 'playlist1',
+      playlistTracks: [
+        {name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4},
+        {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5},
+        {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}
+      ]};
   }
   render() {
     return (
@@ -21,7 +28,8 @@ class App extends React.Component {
           {/*<SearchBar />*/}
         <div className="App-playlist">
           <SearchResults SearchResults={this.state.SearchResults} />
-          {/*<SearchResults /> */}
+          <Playlist playlistName={this.state.playlistName}
+                    playlistTracks={this.state.playlistTracks} />
         </div>
       </div>
     </div>
